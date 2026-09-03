c6526fd846fd125064d4534840e601cd67a7a517
pavanediyakula123 <pavanediyakula@gmail.com>
2026-09-03 14:37:43 +0530

    Day 3 Python class work

---
+++ Day-03/Understanding_comments.py
@@ -0,0 +1,11 @@
+"This is a sample program for understanding comments in python"
+
+n = "This is a string"
+print(n)
+
+'''There is a most unique and well known difference between list and tuple that a
+can be changed or modified at any given point of time but when it comes to tuple it can't be changed or modified
+becaues it is immutable in nature'''
+"""Now the above string can be known as multiline comments or it is used as a
+doc string for the documentation , and a comment is generally used for the purpose
+of explanation of the code that is written"""
